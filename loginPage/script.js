4b764eca0f65feadfbd600d27261dfb75ccf2a45
Mustard Chunky <121204519+MrChunky@users.noreply.github.com>
2024-03-15 14:29:38 -0400

    initial

---
+++ loginPage/script.js
@@ -0,0 +1,17 @@
+console.log("hi")
+
+console.log("this is working ")
+
+function log() {
+    console.log("star")
+}
+
+function confirmPassword(password) {
+    realPassword = "tea"
+
+    if (password == realPassword) {
+        return true
+    } else {
+        return false
+    }
+}
